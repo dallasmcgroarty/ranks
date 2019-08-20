c789f94b5a5d49fdf1d2bde541fe3854f3e18e95
dallasmcgroarty <dallas.mcgroarty@gmail.com>
2019-08-20 14:14:56 -0700

    alert off addscore

--- main.js
+++ main.js
@@ -165,7 +165,6 @@ function subtractScore () {
 
 // add score increment to user score
 function addScore () {
-  alert('plus has been clicked');
   if (!document.querySelector('.card')) {
     console.log('No players added');
     return;
